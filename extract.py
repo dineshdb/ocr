2e78c5deda5fe75313c528984d94625500806dfd
Dinesh Bhattarai <dineshbhattrai0@gmail.com>
2018-02-17 12:45:08 +0545

    Extract image contents

---
+++ extract.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+import sys
+import cv2
+import numpy as np
+
+# Create MSER (Maximally Stable External Regions) object
+mser = cv2.MSER_create()
+
+def draw_contours(img, contours):
+    cv2.polylines(img, contours, 1, (0, 255, 0))
+    for contour in contours:
+        cv2.drawContours(img, [contour], -1, (255, 255, 255), -1)
+    return img
+    
+def find_texts(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    thres = cv2.adaptiveThreshold(blur, 255, 1, 1, 11, 2)
+
+    mask = np.zeros((img.shape[0], img.shape[1], 1), dtype=np.uint8)
+    
+    regions, _ = mser.detectRegions(thres)
+
+    # [Convex Hull](https://en.wikipedia.org/wiki/Convex_hull) gives the smallest convext set that contains the detected letter
+    hulls = [cv2.convexHull(p.reshape(-1, 1, 2)) for p in regions]
+    
+    # Mask and remove all non-text contents
+    mask = draw_contours(mask, hulls)        
+    text_only = cv2.bitwise_and(img, img, mask=mask)
+    
+    cv2.imshow('img', text_only)
+
+    while cv2.waitKey(0) != 27:
+        pass
+    cv2.destroyAllWindows()
+    
+img = cv2.imread("img/1.jpg")
+find_texts(img)
